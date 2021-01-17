14a0b3f81e1ca2c826be34aff171e0bbea058e8b
Olrik van Dijk <ovd@gmx.net>
2021-01-17 14:02:34 +0100

    created the first action

--- packages/marketing/config/webpack.dev.js
+++ packages/marketing/config/webpack.dev.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
@@ -23,7 +23,7 @@ const devConfig = {
     }),
     new HtmlWebpackPlugin({
       template: './public/index.html'
-    })
+    }) 
   ]
 };
 
