9c40f6dec69805f8350a59ba2213167673540617
Olrik van Dijk <ovd@gmx.net>
2021-01-17 17:12:12 +0100

    for rebuild

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,8 @@ import MarketingApp from '../components/MarketingApp';
 export default() => {
   return (
     <div>
-      <h1>Marketing</h1>
+      <h1>Hello Marketing</h1>
+      <hr />
       <MarketingApp />
     </div>
   );
